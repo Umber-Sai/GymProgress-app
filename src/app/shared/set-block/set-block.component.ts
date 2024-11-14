beadd22436efc676cb35b77bfb0fdd69d0a949d3
Kirill Kapralov <Kapralov1501@gmail.com>
2024-11-14 22:21:54 +0200

    senser fix

--- src/app/shared/set-block/set-block.component.ts
+++ src/app/shared/set-block/set-block.component.ts
@@ -1,5 +1,5 @@
 import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { ExerciseSetType } from 'src/app/type/exercise-history.type';
 
 @Component({
@@ -26,24 +26,30 @@ import { ExerciseSetType } from 'src/app/type/exercise-history.type';
     ])
   ]
 })
-export class SetBlockComponent implements OnInit {
+export class SetBlockComponent implements AfterViewInit {
 
   @Input() labels : ExerciseSetType = {w : '', r : ''}
   @Input() values : ExerciseSetType | null = null;
   @Output() deleteSet : EventEmitter<boolean> = new EventEmitter<boolean>()
+  @ViewChild('set', { static: false }) setElement!: ElementRef;
   positionX = 0;
   offsetX = 0;
   isMoove : boolean = false;
+  
 
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    // this.setElement.nativeElement.addEventListener('touchstart', this.onTouchMove.bind(this), { passive: false });
+    this.setElement.nativeElement.addEventListener('touchmove', this.onTouchMove.bind(this), { passive: false });
+    // this.setElement.nativeElement.addEventListener('touchend', this.onTouchMove.bind(this));
   }
+
 
  
 
-  onTouchStart(event: TouchEvent, target : HTMLElement) {
+  onTouchStart(event: TouchEvent) {
     if(!this.values) return
     const touch = event.touches[0];
     this.offsetX = touch.clientX - this.positionX;
@@ -51,20 +57,20 @@ export class SetBlockComponent implements OnInit {
     const initX = touch.clientX;
     const initY = touch.clientY;
     setTimeout(() => {
-        target.addEventListener('touchmove', (event) => {
+        this.setElement.nativeElement.addEventListener('touchmove', (event : TouchEvent) => {
             const offsetY = event.touches[0].clientY - initY;
             const offsetX = event.touches[0].clientX - initX;
             const corner = Math.atan(Math.abs(offsetY) / Math.abs(offsetX)) * 180 / Math.PI;
-            if (corner < 80) {
+            if (corner < 20) {
                 event.preventDefault();
                 (document.activeElement as HTMLElement).blur();
                 this.isMoove = true;
             }
-        }, { once: true })
+        }, { once: true, passive: false  })
     }, 0)
   }
 
-  onTouchMove(event: TouchEvent, target : HTMLElement) {
+  onTouchMove(event: TouchEvent) {
     if(!this.isMoove || !this.values) return
     event.preventDefault(); 
 
@@ -74,10 +80,10 @@ export class SetBlockComponent implements OnInit {
     if(this.positionX < -150) this.positionX = -150;
     if(this.positionX > 0) this.positionX = 0;
 
-    target.style.transform = `translateX(${this.positionX}px)`;
+    this.setElement.nativeElement.style.transform = `translateX(${this.positionX}px)`;
   }
 
-  onTouchEnd(event: TouchEvent, target : HTMLElement) {
+  onTouchEnd() {
     if( !this.values) return
     if(this.positionX < -100) {
       this.deleteSet.next(true);
